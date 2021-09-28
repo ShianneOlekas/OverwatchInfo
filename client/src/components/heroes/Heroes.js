d46677fa4d9ca84525a4c348706b528b8eebb66e
Ana Olekas <86985160+ShianneOlekas@users.noreply.github.com>
2021-09-28 12:06:47 -0400

    added ana through hanzo

--- client/src/components/heroes/Heroes.js
+++ client/src/components/heroes/Heroes.js
@@ -1,20 +1,31 @@
 import './Heroes.scss'
+import { Parallax } from 'react-scroll-parallax';
 
 function Heroes({ heroes }) {
  
     const heroesRendered = heroes?.map((hero) => (
-        <section className='heroes'>
-            <div className={hero.styleClass}>
+        // <section className='heroes'>
+            <div className={[hero.styleClass, 'heroes'].join(" ")}>
+            <Parallax
+            x={[-20, 30]}
+            >
+                <h1 className='heroes__name'>{hero.name}</h1>
                 <img src={hero.image} alt='hero' className='heroes__img'/>
-                <h1 className={hero.styleName}>{hero.name}</h1>
-                <h2>AGE {hero.age}</h2>
-                <p>{hero.tagline}</p>
+            </Parallax>
+            <Parallax
+            x={[90, -10]}
+            >
+                <p className='heroes__tagline'>{hero.tagline}</p>
+            </Parallax>
+            <div className='heroes__info-container'>
+                <h2 className='heroes__age'>AGE {hero.age}</h2>
                 <p>Origin: {hero.origin}</p>
                 <p>Occupation: {hero.occupation}</p>
                 <p>Affiliation: {hero.affiliation}</p>
                 <p>{hero.backstory}</p>
             </div>
-        </section>
+            </div>
+        // </section>
         ));
 
     return (
